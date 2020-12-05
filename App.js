a0b456b4673da94074bffbef12ff28f5c6fa04e2
elham poshtiban <elham@londr.com>
2020-12-05 00:32:07 -0600

    adding image tag to the application

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  let x=1
   return (
-    <View style={styles.container}>
+   
+    <SafeAreaView style={styles.container}>
       <Text> Hi this is Elham phone!</Text>
+      <TouchableOpacity onPress={()=> console.log("this image is clicking")}>
+      <Image source={{
+        width: 200,
+        height: 300,
+        uri: 'https://picsum.photos/200/300'}}/>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
